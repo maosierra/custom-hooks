a19c3a44416d04d568e4e63eecdb251f7e703afb
Mauricio Sierra <mauricio.sierra@globant.com>
2020-07-07 11:30:08 -0500

    Primer commit

---
+++ useFetch.js
@@ -0,0 +1,41 @@
+import { useState, useEffect, useRef } from 'react'
+
+export const useFetch = (url) => {
+
+    const isMounted = useRef(true);
+    const [state, setState] = useState({
+        data: null,
+        loading: true,
+        error: null
+    });
+
+    useEffect(() => {
+        return () => {
+            isMounted.current = false;
+        }
+    }, [])
+
+    useEffect(() => {
+        setState({
+            data: null,
+            loading: true,
+            error: null
+        });
+
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
+                if (isMounted) {
+                    setState({
+                        loading: false,
+                        error: null,
+                        data
+                    });
+                } else {
+                    console.log('no se llamo');
+                }
+            })
+    }, [url])
+
+    return state;
+}
